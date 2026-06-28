5781b93b7703c7d29e57d59ef4e0cb7c0432f1a8
vk-hr <viktor.harhatt@gmail.com>
2026-06-28 17:43:07 +0300

    add nixfmt

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,18 +1,19 @@
 { config, pkgs, ... }:
 
 {
-	home.packages = with pkgs; [
-		git
-		neovim
-		htop
-		fzf
-		foot
-		zed-editor
-		helium
-		fuzzel
-		wl-clipboard
-		xwayland
-		nil
-		nixd
-	];
+  home.packages = with pkgs; [
+    git
+    neovim
+    htop
+    fzf
+    foot
+    zed-editor
+    helium
+    fuzzel
+    wl-clipboard
+    xwayland
+    nil
+    nixd
+    nixfmt
+  ];
 }
--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -1,46 +1,44 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }: {
+  hardware.enableAllFirmware = true;
 
-{
-	hardware.enableAllFirmware = true;
+  networking.networkmanager.enable = true;
 
-	networking.networkmanager.enable = true;
+  services.power-profiles-daemon.enable = true;
 
-	services.power-profiles-daemon.enable = true;
+  hardware = {
+    graphics.enable = true;
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+  };
 
-	hardware = {
-		graphics.enable = true;
-		bluetooth = {
-			enable = true;
-			powerOnBoot = true;
-		};
-	};
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-	};
+  security.polkit.enable = true;
 
-	security.polkit.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri";
+        user = "greeter";
+      };
+    };
+  };
 
-	services.greetd = {
-    		enable = true;
-    		settings = {
-      			default_session = {
-        			command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri";
-        			user = "greeter";
-			};
-    		};
-  	};
+  programs.niri.enable = true;
 
-	programs.niri.enable = true;
-
-  	xdg.portal = {
-    		enable = true;
-    		extraPortals = with pkgs; [
-      			xdg-desktop-portal-wlr
-      			xdg-desktop-portal-gtk
-    		];
-	};
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+  };
 }
