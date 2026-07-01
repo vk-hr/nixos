9b3185226caee2d72c26db6949d662ef5f487d1c
vk-hr <viktor.harhatt@gmail.com>
2026-07-01 18:38:17 +0300

    disable ghostty window decoration

--- home/desktop/ghostty.nix
+++ home/desktop/ghostty.nix
@@ -2,11 +2,12 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      theme = "GruvboxDark";
+      theme = "Gruvbox Dark";
       font-family = "monospace";
       font-size = 12;
       window-padding-x = 8;
       window-padding-y = 4;
+      window-decoration = false;
     };
   };
 }
