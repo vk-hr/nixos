97fdab4f06a615e52fcbc483e4a64bf512f8145b
vk-hr <viktor.harhatt@gmail.com>
2026-07-07 04:30:04 +0300

    add volume and backlight modules to waybar

--- home/desktop/waybar.nix
+++ home/desktop/waybar.nix
@@ -36,11 +36,37 @@
           "tray"
           "network"
           "battery"
+          "backlight"
+          "pulseaudio"
         ];
 
         "niri/workspaces".format = "{index}";
 
         clock.format = "{:%H:%M  %a %b %d}";
+
+        backlight = {
+          format = "{icon}  {percent}%";
+          format-icons = [
+            "󰖨"
+            "󰖨"
+          ];
+          on-scroll-up = "brightnessctl set +5%";
+          on-scroll-down = "brightnessctl set 5%-";
+        };
+
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          format-muted = "󰖸  muted";
+          format-icons = {
+            headphone = "󰋋";
+            default = [
+              "󰕿"
+              "󰖀"
+              "󰕾"
+            ];
+          };
+          on-click = "pavucontrol";
+        };
 
         network = {
           format-wifi = "󰖩  {essid} ({signalStrength}%)";
