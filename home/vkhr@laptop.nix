393f4d5d18bd5b806c091f52323bec3173dc0026
vk-hr <viktor.harhatt@gmail.com>
2026-07-16 03:09:15 +0300

    enable home pointer cursor explicitly

--- home/vkhr@laptop.nix
+++ home/vkhr@laptop.nix
@@ -5,6 +5,12 @@
     username = "vkhr";
     homeDirectory = "/home/vkhr";
     stateVersion = "26.05";
+    pointerCursor = {
+      enable = true;
+      name = lib.mkDefault "Adwaita";
+      package = lib.mkDefault pkgs.adwaita-icon-theme;
+      size = lib.mkDefault 24;
+    };
   };
 
   programs.home-manager.enable = true;
