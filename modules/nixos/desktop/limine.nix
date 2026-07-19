5b75785ab84ecf1f83b22eaf84c35fde80eac31a
vk-hr <viktor.harhatt@gmail.com>
2026-07-19 05:08:44 +0300

    style limine menu

--- modules/nixos/desktop/limine.nix
+++ modules/nixos/desktop/limine.nix
@@ -1,12 +1,19 @@
 { config, lib, ... }:
+let
+  colors = config.lib.stylix.colors;
+in
 {
   boot.loader.limine.style = {
     wallpapers = lib.mkForce [ ];
-    backdrop = config.lib.stylix.colors.base00;
+    backdrop = colors.base00;
+    graphicalTerminal.font.scale = "2x2";
     interface = {
-      branding = "";
-      helpHidden = true;
-      resolution = "2880x1800x32";
+      branding = "NixOS";
+      brandingColor = colors.base0B;
+      helpHidden = false;
+      helpColor = colors.base0B;
+      helpColorBright = colors.base0A;
+      resolution = "2880x1800";
     };
   };
 }
